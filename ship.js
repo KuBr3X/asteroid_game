db2ecf6bf921a9bd30219eaef24e394bbd06b705
Sander J <home978.sj@gmail.com>
2021-01-21 08:55:08 +0100

    Updated

--- ship.js
+++ ship.js
@@ -1,66 +1,109 @@
 function Ship() {
-    this.pos = createVector(width / 2, height / 2);
+    this.pos = createVector(width/2, height/2);
     this.r = 20;
     this.heading = 0;
-    this.rotation = 0;
+    this.rotation = 0; 
     this.vel = createVector(0, 0);
     this.isBoosting = false;
-  
-    this.boosting = function(b) {
-      this.isBoosting = b;
-    };
-  
+
+     this.boosting = function(b){
+          this.isBoosting = b;
+     }
+
     this.update = function() {
-      if (this.isBoosting) {
-        this.boost();
-      }
-      this.pos.add(this.vel);
-      this.vel.mult(0.99);
-    };
-  
-    this.boost = function() {
-      var force = p5.Vector.fromAngle(this.heading);
-      force.mult(0.1);
-      this.vel.add(force);
-    };
-  
+        if (this.isBoosting) {
+            this.boost();
+    }
+        this.pos.add(this.vel); 
+        this.vel. mult(0.99)
+    } 
+
+    this.boost = function (){
+         var force = p5.Vector.fromAngle(this.heading);
+         force.mult(0.1);
+         this.vel.add(force);
+    }
+
     this.hits = function(asteroid) {
-      var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
-      if (d < this.r + asteroid.r) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-  
+        var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+        if (d < this.r + asteroid.r) {
+            return true;
+        } else {
+            return false
+        }
+    }
+
+
     this.render = function() {
-      push();
-      translate(this.pos.x, this.pos.y);
-      rotate(this.heading + PI / 2);
-      fill(0);
-      stroke(255);
-      triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
-      pop();
-    };
-  
-    this.edges = function() {
-      if (this.pos.x > width + this.r) {
-        this.pos.x = -this.r;
-      } else if (this.pos.x < -this.r) {
-        this.pos.x = width + this.r;
-      }
-      if (this.pos.y > height + this.r) {
-        this.pos.y = -this.r;
-      } else if (this.pos.y < -this.r) {
-        this.pos.y = height + this.r;
-      }
-    };
-  
+     if (isUp) { //Tasten W giver boost
+        push ();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading + PI/2);
+        fill(0);
+        stroke(255);
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r); 
+                    //Boost farve animation
+        stroke(255);
+        fill (color = "blue")  
+        triangle(-this.r, this.r, this.r, this.r, 0, this.r*2);
+        noStroke(0);
+        fill (color = "lightblue")  
+        triangle(-this.r, this.r, this.r, this.r, 0, this.r*1.5);
+        noStroke();
+        fill (color = "blue")  
+        triangle(-this.r, this.r, this.r, this.r, 0, this.r*1.25);
+            } else  //"else" til når boost ikke bliver brugt, så slår den fra
+                {
+        push ();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading + PI/2);
+        fill(0);
+        stroke(255);
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r); 
+        }
+
+    }
+
+    this.movement = function() { //Boost movement
+        if (isUp) {
+            push();
+            ship.boosting;
+            pop();
+        }
+        ship.boosting(isUp)
+
+        if (isRight) {
+            ship.setRotation (0.1);
+        }
+
+        if (isLeft) {
+            ship.setRotation (-0.1);
+        }
+        if (!isLeft && !isRight || isLeft && isRight) {
+            ship.setRotation (0);
+        }
+
+    }
+
+    this.edges = function() { //Koden der gør at skibet bliver inde på skærmen.
+        if (this.pos.x > width +  this.r) {
+            this.pos.x = -this.r;     
+        } else if (this.pos.x < -this.r) {
+            this.pos.x = width + this.r;
+        }
+
+        if (this.pos.y > height + this.r) {
+            this.pos.y = -this.r;   
+        } else if (this.pos.y < -this.r) {
+            this.pos.y = height + this.r;
+        }
+    }
+
     this.setRotation = function(a) {
-      this.rotation = a;
-    };
-  
+        this.rotation = a;
+    }
+
     this.turn = function() {
-      this.heading += this.rotation;
-    };
-  }
+        this.heading += this.rotation;
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,68 +1,89 @@
+//Forskellige variabler til de diverse ting der skal kodes
 var ship;
 var asteroids = [];
 var lasers = [];
+var isRight = false;
+var isLeft = false;
+var isUp = false; 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ship = new Ship();
-  for (var i = 0; i < 5; i++) {
+
+function setup() { //Kode til canvasset som også bestemmer hvor mange asteroider der bliver tilføjet
+    createCanvas(windowWidth, windowHeight);
+    ship = new Ship();
+    for (var i = 0; i < 20; i++) { //Denne kode er den der bestemmer hvor mange asteroider der er 
     asteroids.push(new Asteroid());
-  }
+} 
 }
 
-function draw() {
-  background(0);
+function draw(){
+    background(0);
+    
 
-  for (var i = 0; i < asteroids.length; i++) {
-    if (ship.hits(asteroids[i])) {
-      console.log('ooops!');
-    }
-    asteroids[i].render();
-    asteroids[i].update();
-    asteroids[i].edges();
-  }
-
-  for (var i = lasers.length - 1; i >= 0; i--) {
-    lasers[i].render();
-    lasers[i].update();
-    if (lasers[i].offscreen()) {
-      lasers.splice(i, 1);
-    } else {
-      for (var j = asteroids.length - 1; j >= 0; j--) {
-        if (lasers[i].hits(asteroids[j])) {
-          if (asteroids[j].r > 10) {
-            var newAsteroids = asteroids[j].breakup();
-            asteroids = asteroids.concat(newAsteroids);
-          }
-          asteroids.splice(j, 1);
-          lasers.splice(i, 1);
-          break;
+    for (var i = 0; i < asteroids.length; i++) { //Kode der renderer asteroider
+        if (ship.hits(asteroids[i])) {
+            console.log("ups");
         }
-      }
+        asteroids[i].render();
+        asteroids[i].update();
+        asteroids[i].edges();
+
     }
-  }
 
-  console.log(lasers.length);
-
-  ship.render();
-  ship.turn();
-  ship.update();
-  ship.edges();
+    for (var i = lasers.length-1; i >=0; i--) { //Kode der renderer laser skud 
+        lasers[i].render();
+        lasers[i].update();
+        if (lasers[i].offscreen()) {
+            lasers.splice(i, 1);
+        } else {
+        for (var j = asteroids.length-1; j >= 0; j--){ //Kode der ødelægger asteroiderne 
+        if (lasers[i].hits(asteroids[j])) {
+         if (asteroids[j].r > 20){
+         var newasteroids = asteroids[j].breakup();
+         asteroids = asteroids.concat(newasteroids);
+         } 
+         asteroids.splice(j, 1);
+         lasers.splice(i, 1);
+         break;
+        }
+    }
+    }
 }
 
-function keyReleased() {
-  ship.setRotation(0);
-  ship.boosting(false);
-}
 
-function keyPressed() {
-  if (key == ' ') {
-    lasers.push(new Laser(ship.pos, ship.heading));
-  } else if (keyCode == RIGHT_ARROW) {
-    ship.setRotation(0.1);
-  } else if (keyCode == LEFT_ARROW) {
-    ship.setRotation(-0.1);
-  } else if (keyCode == UP_ARROW) {
-    ship.boosting(true);
-  }
+        ship.render();
+        ship.turn();
+        ship.update(); 
+        ship.edges();
+        ship.movement();
+}  
+
+function keyReleased (){
+    ship.setRotation (0);
+    ship.boosting(false);
+
+    if (keyCode == 68) {
+        isRight = false
+    } 
+    if (keyCode == 65) {
+        isLeft = false
+    }
+    if (keyCode == 87) {
+        isUp = false
+    }
+
+ }
+          //Koder til hvilke taster der skal bruges til laser og movement
+function keyPressed () {
+    if (keyCode == 32){
+        lasers.push(new Laser(ship.pos, ship.heading));
+    }  
+    if (keyCode == 68) {
+        isRight = true
+    }
+    if (keyCode == 65) {
+        isLeft = true
+    }
+    if (keyCode == 87) {
+        isUp = true
+    }
 }
